bae05e0be4deb75c3e032a6d32fafff6668312a1
Pau Fernández <pau.fernandez@upc.edu>
2024-07-22 11:49:06 +0200

    Prepare deploy

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,3 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+import { PrismaLibSQL } from "@prisma/adapter-libsql"
+import { createClient } from "@libsql/client"
 
-export const db = new PrismaClient();
+const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env
+if (!TURSO_DATABASE_URL) {
+  throw new Error(`Missing TURSO_DATABASE_URL env variable!`)
+}
+if (!TURSO_AUTH_TOKEN) {
+  throw new Error(`Missing TURSO_AUTH_TOKEN env variable`)
+}
+
+const libsql = createClient({
+  url: TURSO_DATABASE_URL,
+  authToken: TURSO_AUTH_TOKEN,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+export const db = new PrismaClient({ adapter })
